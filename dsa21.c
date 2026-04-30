202c810e6e121ffbd33b2fa88fc8233932d19f9a
kratijain12-sketch <kratijainviiia@gmail.com>
2026-04-30 14:54:40 +0530

    twenty first commit

--- dsa21.c
+++ dsa21.c
@@ -1,3 +1,20 @@
+// Problem: Create and Traverse Singly Linked List
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the result
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
 #include <stdio.h>
 #include <stdlib.h>
 
